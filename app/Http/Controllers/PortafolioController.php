c22738d4a6f64a5cdfba169bcc19f35a08ada3ea
unknown <1997.al.hdz@gmail.com>
2023-02-27 12:02:23 -0600

    pagina

---
+++ app/Http/Controllers/PortafolioController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proyecto;
+use Illuminate\Http\Request;
+
+class PortafolioController extends Controller
+{
+    public function index()
+    {
+        $proyectos = Proyecto::paginate();
+         return view('welcome', compact('proyectos'));
+
+        /*return view('proyecto.index', compact('proyectos'))
+                    ->with('i', (request()->input('page', 1) - 1) * $proyectos->perPage());*/
+    }
+}
